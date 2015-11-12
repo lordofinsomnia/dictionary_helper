8969e4f9fea03802ad62b6e3c56882c1ed3e41af
lordofinsomnia <mickom669@yahoo.com>
2015-11-12 19:32:26 +0100

    cleanup TDD-a

--- dictionaryHelper_test.go
+++ dictionaryHelper_test.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	//	"fmt"
 	"github.com/go-martini/martini"
-	//	"github.com/martini-contrib/render"
 	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
 	"net/http/httptest"
-	//	"strings"
 	"testing"
 )
-
-/*
-func startTestServer(path string, pathHandler martini.Handler) *httptest.ResponseRecorder {
-	webApp := martini.Classic()
-	curPath := path
-	webApp.Get(curPath, pathHandler)
-
-	webApp.Use(render.Renderer())
-
-	return getResponse(webApp, curPath)
-}*/
 
 func getResponse(webApp *martini.ClassicMartini, path string) *httptest.ResponseRecorder {
 	request, _ := http.NewRequest("GET", path, nil)
@@ -29,56 +15,6 @@ func getResponse(webApp *martini.ClassicMartini, path string) *httptest.Response
 	return response
 }
 
-/*
-func traceError(testName string, t *testing.T, response *httptest.ResponseRecorder) {
-	t.Error(testName, " failed!")
-	t.Error("	code: ", response.Code)
-	t.Error("	body len: ", len(response.Body.String()))
-	t.Error("	body: ", response.Body.String())
-}
-
-func TestRoutesSimulator(t *testing.T) {
-	for _, curRoute := range routes {
-		response := startTestServer(curRoute.path, curRoute.funcHandler)
-		if response.Code != http.StatusOK {
-			traceError(curRoute.name, t, response)
-		}
-	}
-}
-
-func TestServerSettings(t *testing.T) {
-	webApp = nil
-	configureServer()
-	if webApp == nil {
-		t.Error("TestRoutes failed web=nil")
-	}
-}
-
-func TestRouteHaveCaption(t *testing.T) {
-	webApp = nil
-	configureServer()
-	for _, curRoute := range routes {
-		response := getResponse(webApp, curRoute.path)
-		fmt.Println("path: " + curRoute.path)
-		fmt.Println("body: " + response.Body.String())
-		fmt.Println("caption: " + curRoute.caption)
-		if strings.Contains(response.Body.String(), curRoute.caption) == false {
-			traceError(curRoute.name, t, response)
-		}
-	}
-}
-
-func TestRouteLinkWorks(t *testing.T) {
-	webApp = nil
-	configureServer()
-	for _, curRoute := range routes {
-		response := getResponse(webApp, curRoute.path)
-		if response.Code != http.StatusOK {
-			traceError(curRoute.name, t, response)
-		}
-	}
-}
-*/
 func TestUtils(t *testing.T) {
 	Convey("Testing utils func", t, func() {
 		Convey("htmlHeader func", func() {
