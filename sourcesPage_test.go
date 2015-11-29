30afeb23e63acb318ba82a5cb44da05015361cb1
lordofinsomnia <mickom669@yahoo.com>
2015-11-29 20:17:20 +0100

    sourcesPage tests in separate file

---
+++ sourcesPage_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSources(t *testing.T) {
+	webApp = nil
+	Convey("StartServer", t, func() {
+		configureServer()
+		response := getResponse(webApp, "GET", "/sources")
+		responseStr := response.Body.String()
+		Convey("Sources works", func() {
+			Convey("Has all gui items", func() {
+				createSourcePage()
+				Convey("Has source groupbox", func() {
+					Convey("Has groupbox caption", func() {
+						So(responseStr, ShouldContainSubstring, grpSource)
+					})
+					Convey("Has caption", func() {
+						Convey("Has caption label", func() {
+							So(responseStr, ShouldContainSubstring, lblCaption)
+						})
+						Convey("Has caption editbox", func() {
+							So(responseStr, ShouldContainSubstring, edtCaption)
+						})
+					})
+					Convey("Has year", func() {
+						Convey("Has year label", func() {
+							So(responseStr, ShouldContainSubstring, lblYear)
+						})
+						Convey("Has year editbox", func() {
+							So(responseStr, ShouldContainSubstring, edtYear)
+						})
+					})
+					Convey("Has shortname", func() {
+						Convey("Has shortname label", func() {
+							So(responseStr, ShouldContainSubstring, lblShortName)
+						})
+						Convey("Has shortname editbox", func() {
+							So(responseStr, ShouldContainSubstring, edtShortName)
+						})
+					})
+				})
+			})
+		})
+	})
+}
